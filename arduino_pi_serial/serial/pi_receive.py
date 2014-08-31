1c7acf058364ecec945ee02932b2bc746bed70d3
Lorenzo Pons <lorenzo.pons@hotmail.it>
2014-08-31 23:47:17 +0000

    added exceptions tracking to pi_receive.py

--- arduino_pi_serial/serial/pi_receive.py
+++ arduino_pi_serial/serial/pi_receive.py
@@ -1,14 +1,29 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0', 115200)
 ser.open()
 
 try:
-	while 1:
-		response = ser.readline()
-		print response
-		time.sleep(0.25)
+	while True:
+		try:
+			if (ser.inWaiting() > 0):
+				response = ser.readline()
+				print response
+				time.sleep(0.05)
+
+		except IOError as e:
+			print 'I/O Exception ({0}): {1}'.format(e.errno, e.strerror)
+			print 'Disable default console serial login'
+			print 'Closing and re-opening serial port'
+			ser.close()
+			ser.open()
+		except OSError as e:
+			print 'OS Exception ({0}): {1}'.format(e.errno, e.strerror)
+			print 'Disable default console serial login'
+			print 'Closing and re-opening serial port'
+			ser.close()
+			ser.open()
 
 except KeyboardInterrupt:
         ser.close()
